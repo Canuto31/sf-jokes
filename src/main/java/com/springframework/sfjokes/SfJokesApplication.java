1699d3cbf52bc6c18f9184c8613aaeb652c2b81e
Juan Moya <jcmt.personal.vg@gmail.com>
2023-03-14 10:11:26 -0500

    Project creation

---
+++ src/main/java/com/springframework/sfjokes/SfJokesApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sfjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/sfjokes/SfJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
